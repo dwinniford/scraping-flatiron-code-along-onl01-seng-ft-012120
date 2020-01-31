cd3cea7f65c53b2d297450fc170a229fa2fb211a
Dave Winniford <dwinniford@gmail.com>
2020-01-31 13:20:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    open-uri()
+    open-uri(http://learn-co-curriculum.github.io/site-for-scraping/courses)
   end 
   
   def get_courses
